30505c579600afc5c136c14689bb014add78d0ad
Yevhenii Prasol <mefgner@gmail.com>
2025-09-12 14:13:26 +0200

    Add methods for user retrieval by ID and user verification with password

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -16,3 +16,14 @@ async def create_user(session: AsyncSession, username: str, password: str, email
 async def get_user_by_email(session: AsyncSession, email: str) -> User | None:
     result = await session.execute(select(User).where(User.email == email).limit(1))
     return result.scalars().first()
+
+
+async def get_user_by_id(session: AsyncSession, user_id: str) -> User | None:
+    return await session.get(User, user_id)
+
+
+async def verify_user(session: AsyncSession, user_id: str, password: str) -> bool:
+    user = await get_user_by_id(session, user_id)
+    if user:
+        return hashing.verify_password(password, user.password_hash)
+    return False
