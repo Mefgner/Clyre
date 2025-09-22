b91e777ac5a4ad823326c9f824cd34d927b4a520
Yevhenii Prasol <mefgner@gmail.com>
2025-09-22 13:03:10 +0200

    Refactor: move Uvicorn logging configuration to `utils.fields` for better reusability and code simplification.

--- backend/manager.py
+++ backend/manager.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 from app import app
-from utils import env
+from utils import env, fields
 
 
 def main():
@@ -9,38 +9,7 @@ def main():
         app,
         host=env.HOST,
         port=env.PORT,
-        log_config={
-            "version": 1,
-            "disable_existing_loggers": False,
-            "formatters": {
-                "default": {
-                    "()": "uvicorn.logging.DefaultFormatter",
-                    "fmt": "%(levelname)s:uvicorn:%(message)s",
-                    "use_colors": None,
-                },
-                "access": {
-                    "()": "uvicorn.logging.AccessFormatter",
-                    "fmt": '%(levelname)s:uvicorn:"%(request_line)s":%(status_code)s',
-                },
-            },
-            "handlers": {
-                "default": {
-                    "formatter": "default",
-                    "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",
-                },
-                "access": {
-                    "formatter": "access",
-                    "class": "logging.StreamHandler",
-                    "stream": "ext://sys.stdout",
-                },
-            },
-            "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
-                "uvicorn.error": {"level": "INFO"},
-                "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
-            },
-        },
+        log_config=fields.UVICORN_LOGGING_CONFIG,
     )
 
 
