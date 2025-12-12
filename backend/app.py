e9b175322538138fee8abb8617eac1d12d268a3e
Yevhenii Prasol <mefgner@gmail.com>
2025-12-12 19:50:32 +0100

    Enhance logging with `colorlog`, add an exception handler, expand allowed CORS origins, and enable llama pipeline lifecycle management.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 import logging
 
+import colorlog
 import sys
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 import db
 from api import views
+from pipelines import llama
 from utils import cfg, downloader, env
 
 logging.getLogger("httpcore").setLevel(logging.WARNING)
@@ -14,17 +16,26 @@ logging.getLogger("aiosqlite").setLevel(logging.WARNING)
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
 Logger = logging.getLogger()
-Logger.handlers.clear()
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(cfg.get_logging_level())
 handler.setFormatter(
-    logging.Formatter(
-        fmt="%(asctime)s - %(levelname)s - %(name)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    colorlog.ColoredFormatter(
+        fmt="%(thin_light_white)s%(asctime)s%(reset)s - %(log_color)s%(levelname)s%(reset)s - %(name)s: %(thin_light_green)s%(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "purple",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
     )
 )
 
 Logger.addHandler(handler)
+
+Logger.info("Pre-downloading necessary files...")
 
 downloader.predownload("binaries.yaml", "models.yaml")
 
@@ -34,6 +45,8 @@ app.include_router(views.api_router, prefix="/api")
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://0.0.0.0",
+    "http://192.168.137.1:3000",
 ]
 
 app.add_middleware(
@@ -44,7 +57,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.exception_handler(Exception)
+async def handle_exception(request, exc):
+    Logger.error(f"Unhandled exception: \n%s\n\n Request: %s", exc, request, exc_info=True)
+    raise exc
+
+
 app.add_event_handler("startup", db.get_session_manager().init_models)
-# app.add_event_handler("startup", llama.get_current_llama_pipeline().wait_for_startup)
+app.add_event_handler("startup", llama.get_current_llama_pipeline().wait_for_startup)
 app.add_event_handler("shutdown", db.get_session_manager().close)
-# app.add_event_handler("shutdown", llama.get_current_llama_pipeline().close)
+app.add_event_handler("shutdown", llama.get_current_llama_pipeline().close)
